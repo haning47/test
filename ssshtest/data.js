de4e2f766ee79be9492d874cd51af7bd6ff8d555
haning47 <haning@go.edu.tw>
2024-01-01 00:20:32 +0800

    Initial commit

---
+++ ssshtest/data.js
@@ -0,0 +1,140 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "校門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15976736312654438,
+          "pitch": -0.06944767327834356,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.5072623130945857,
+          "pitch": -0.09246182108231693,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0463297604620188,
+          "pitch": -0.1991650130010001,
+          "title": "松山高中校門",
+          "text": "松山高中<div>改制於民國78年</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "川堂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 2.41686094526421,
+        "pitch": 0.3076750386045184,
+        "fov": 1.2978488763571745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.671073526709712,
+          "pitch": 0.1937539842304119,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.5967688563854914,
+          "pitch": 0.47070104622940256,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.575889582714537,
+          "pitch": 0.5442256219514352,
+          "title": "川堂",
+          "text": "校門口通往操場的走道"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "操場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5421977286931092,
+          "pitch": -0.13794577752031145,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.5392481693788369,
+          "pitch": 0.08880023521273017,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.14414813806760307,
+          "pitch": 0.38877260849982953,
+          "title": "操場",
+          "text": "<span style=\"font-size:12.0pt;mso-bidi-font-size:11.0pt;\nfont-family:&quot;新細明體&quot;,serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:\nminor-latin;mso-fareast-theme-font:minor-fareast;mso-hansi-font-family:Calibri;\nmso-hansi-theme-font:minor-latin;mso-bidi-font-family:&quot;Times New Roman&quot;;\nmso-bidi-theme-font:minor-bidi;mso-ansi-language:EN-US;mso-fareast-language:\nZH-TW;mso-bidi-language:AR-SA\">操場是師生運動及集會的地點</span>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
